7601db794b97131799008e5f2afeb0b2f1e56be6
Cristian Arcadi <arduoscri@gmail.com>
2022-04-20 18:20:24 +0200

    aggiunta viewbinding, aggiunta login firebase, aggiunta button per testing purposes nel main per reindirizzare alle pagine, aggiunta controllo nel main se l'utente è già loggato

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.ActivityRegisterPageBinding
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var analytics: FirebaseAnalytics
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Obtain the FirebaseAnalytics instance.
         analytics = Firebase.analytics
+        //Obtain the Firebase auth
+        auth = Firebase.auth;
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerMain.setOnClickListener {
+            val intent = Intent(this,RegisterPage::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginMain.setOnClickListener {
+            val intent = Intent(this,LoginPage::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    //check if user is logged in
+    //funzione per vedere se l'utente è già loggato
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser!= null){
+            Toast.makeText(this, "Logged in ", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/RegisterPage.kt
+++ app/src/main/java/com/example/myapplication/RegisterPage.kt
@@ -2,10 +2,44 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityRegisterPageBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterPage : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding : ActivityRegisterPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_page)
+        binding = ActivityRegisterPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            createAccount()
+        }
+        auth = FirebaseAuth.getInstance()
+    }
+
+
+
+    fun createAccount() {
+        val email = binding.txtEmail.text.toString()
+        val psw = binding.txtPsw.text.toString()
+        if (binding.txtEmail.text.toString().equals("") || binding.txtPsw.text.toString().equals("")
+        ) {
+            Toast.makeText(this, "Controlla i dati inseriti", Toast.LENGTH_SHORT).show()
+        } else {
+            auth.createUserWithEmailAndPassword(email, psw).addOnCompleteListener{ task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Utente registrato correttamente", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(this, "Errore nella registrazione", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
