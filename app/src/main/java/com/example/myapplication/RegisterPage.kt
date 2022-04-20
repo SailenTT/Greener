565755f6017728f0a646eae4070fe017dfd220c8
David Poletti <david.poletti3@gmail.com>
2022-04-20 19:38:45 +0200

    Aggiunto backend Login

--- app/src/main/java/com/example/myapplication/RegisterPage.kt
+++ app/src/main/java/com/example/myapplication/RegisterPage.kt
@@ -6,10 +6,13 @@ import android.widget.Toast
 import com.example.myapplication.databinding.ActivityRegisterPageBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class RegisterPage : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
     private lateinit var binding : ActivityRegisterPageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,21 +24,27 @@ class RegisterPage : AppCompatActivity() {
             createAccount()
         }
         auth = FirebaseAuth.getInstance()
+
+        database=Firebase.database.reference
     }
 
 
 
     fun createAccount() {
+        val name= binding.txtNome.text.toString()
         val email = binding.txtEmail.text.toString()
         val psw = binding.txtPsw.text.toString()
         if (binding.txtEmail.text.toString().equals("") || binding.txtPsw.text.toString().equals("")
-        ) {
+            ||name.equals("")) {
             Toast.makeText(this, "Controlla i dati inseriti", Toast.LENGTH_SHORT).show()
         } else {
             auth.createUserWithEmailAndPassword(email, psw).addOnCompleteListener{ task ->
                 if (task.isSuccessful) {
                     Toast.makeText(this, "Utente registrato correttamente", Toast.LENGTH_SHORT)
                         .show()
+                    val userUid=task.result.user!!.uid
+
+                    database.child("users").child(userUid).child("username").setValue(name)
                 } else {
                     Toast.makeText(this, "Errore nella registrazione", Toast.LENGTH_SHORT).show()
                 }
