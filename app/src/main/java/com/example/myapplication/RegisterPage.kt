5a7c87a466bef3c99eb484fa6c51c60aba891c1d
Cristian Arcadi <arduoscri@gmail.com>
2022-04-21 15:50:06 +0200

    Aggiunta facebook login, ancora da raffinare

--- app/src/main/java/com/example/myapplication/RegisterPage.kt
+++ app/src/main/java/com/example/myapplication/RegisterPage.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityRegisterPageBinding
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -15,22 +21,62 @@ class RegisterPage : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var database: FirebaseDatabase
     private lateinit var binding : ActivityRegisterPageBinding
-
+    private lateinit var callbackManager : CallbackManager
+    val TAG = "FACEBOOKTAG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+         callbackManager = CallbackManager.Factory.create()
+
+        auth = FirebaseAuth.getInstance()
+        database = Firebase.database("https://ecoapp-706b8-default-rtdb.europe-west1.firebasedatabase.app/")
 
         binding.btnRegister.setOnClickListener {
             createAccount()
         }
-        auth = FirebaseAuth.getInstance()
 
-        database = Firebase.database("https://ecoapp-706b8-default-rtdb.europe-west1.firebasedatabase.app/")
+        binding.btnLoginFacebook.registerCallback(callbackManager,object : FacebookCallback<LoginResult>{
+            override fun onSuccess(loginResult: LoginResult){
+                Log.d(TAG,"facebook:onSuccess$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+            }
 
-        //database=Firebase.database.reference
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+            }
+        })
     }
 
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    Toast.makeText(this, "SUCCESS", Toast.LENGTH_SHORT).show()
+                } else {
+                    // Sign in fail
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
 
 
     fun createAccount() {
@@ -52,6 +98,7 @@ class RegisterPage : AppCompatActivity() {
             binding.edtPsw.setError("Check password")
             return
         }
+
 
         auth.createUserWithEmailAndPassword(email, psw).addOnCompleteListener { task ->
             if (task.isSuccessful) {
