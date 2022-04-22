da083af68b5357f647f532d20069e88bc50526af
cristiandenicola <cristiandenicola009@gmail.com>
2022-04-22 16:17:36 +0200

    MODIFICA: modifiche UI

---
+++ app/src/main/java/com/eco/app/HomeWindow.kt
@@ -0,0 +1,11 @@
+package com.eco.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeWindow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_window)
+    }
+}
