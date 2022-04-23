cece1e38dc575d66f59fe412272577d6d5460613
Cristian Arcadi <arduoscri@gmail.com>
2022-04-23 18:03:56 +0200

    funzione 7 giorni

--- app/src/main/java/com/eco/app/HomeWindow.kt
+++ app/src/main/java/com/eco/app/HomeWindow.kt
@@ -1,11 +1,34 @@
 package com.eco.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.eco.app.databinding.ActivityHomeWindowBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeWindow : AppCompatActivity() {
+    private lateinit var binding : ActivityHomeWindowBinding
+    //COSTANTI
+    //placeholders, li useremo per costruire la ui
+    companion object{
+        val CARTA = "carta"
+        val PLASTICA = "plastica"
+        val VETRO = "vetro"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_window)
+        binding = ActivityHomeWindowBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+            //CODICE PER CREARE LA LISTVIEW, PRENDE I PRIMI 7 GIORNI A PARTIRE DA MO
+            val dateFormat= SimpleDateFormat("EEE d MMM yyyy")
+            for (i in 0..7) {
+                val cal=Calendar.getInstance()
+                cal.add(Calendar.DATE,i)
+                Log.d("Calendar", dateFormat.format(cal.time))
+            }
+        }
     }
-}
+
