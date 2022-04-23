689435a399d7f510d0f47b6af6a8d2b849d4aeae
Cristian Arcadi <arduoscri@gmail.com>
2022-04-23 18:31:56 +0200

    Aggiunta  quiz template

---
+++ app/src/main/java/com/eco/app/quiz_activity.kt
@@ -0,0 +1,11 @@
+package com.eco.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class quiz_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+}
