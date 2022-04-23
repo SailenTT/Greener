e7c7ecb4d969008382f84d022e8265b5791c9ae2
Cristian Arcadi <arduoscri@gmail.com>
2022-04-23 14:19:57 +0200

    debug activity per testing, pulizia codice mainacti

--- app/src/main/java/com/eco/app/MainActivity.kt
+++ app/src/main/java/com/eco/app/MainActivity.kt
@@ -1,54 +1,13 @@
 package com.eco.app
-
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.eco.app.databinding.ActivityMainBinding
-import com.facebook.AccessToken
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-
 class MainActivity : AppCompatActivity() {
-    private lateinit var analytics: FirebaseAnalytics
-    private lateinit var auth: FirebaseAuth
-
-    private lateinit var binding : ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
-
-        // Obtain the FirebaseAnalytics instance.
-        analytics = Firebase.analytics
-        //Obtain the Firebase auth
-        auth = Firebase.auth;
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        /*binding.registerMain.setOnClickListener {
-            val intent = Intent(this, RegisterPage::class.java)
-            startActivity(intent)
-        }
-
-        binding.loginMain.setOnClickListener {
-            val intent = Intent(this,LoginPage::class.java)
-            startActivity(intent)
-
-        }
-
-        binding.btnLogout.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-
-        }*/
 
     }
 
@@ -57,7 +16,8 @@ class MainActivity : AppCompatActivity() {
     public override fun onStart() {
         super.onStart()
 
-        val intent=Intent(this,LoginPage::class.java)
+        //REDIRECTO AI BUTTON PER DEBUG E TESTING ZIO PORK, NELLA VERSIONE FINALE VA ALLA LOGIN
+        val intent=Intent(this,debug_activity::class.java)
         startActivity(intent)
 
         /*val currentUser = auth.currentUser
