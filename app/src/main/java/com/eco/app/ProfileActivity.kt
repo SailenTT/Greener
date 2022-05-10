8a8ebca49be25b6a21e2190761d5545c78f78334
Cristian Arcadi <arduoscri@gmail.com>
2022-05-10 19:41:13 +0200

    implementazione inserimento immmagine di profilo da parte dell'utente (su emulatore va, su tel no, quindi da rivedere)

--- app/src/main/java/com/eco/app/ProfileActivity.kt
+++ app/src/main/java/com/eco/app/ProfileActivity.kt
@@ -1,11 +1,65 @@
 package com.eco.app
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.eco.app.databinding.ActivityProfileBinding
+
+private lateinit var binding: ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    val register = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            Toast.makeText(this, "APPOSTO REGISTER LAUNCHER", Toast.LENGTH_SHORT).show()
+            val imguri: Uri? = it.data?.data
+            binding.imgProfile.setImageURI(imguri)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        checkPermissionForImage()
+        binding.imgProfile.setOnClickListener {
+            pickImage()
+        }
+
+    }
+
+    private fun checkPermissionForImage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if ((checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+                && (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+            ) {
+                val permission = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                val permissionCoarse = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+                requestPermissions(
+                    permission,
+                    1001
+                ) // GIVE AN INTEGER VALUE FOR PERMISSION_CODE_READ LIKE 1001
+                requestPermissions(
+                    permissionCoarse,
+                    1002
+                ) // GIVE AN INTEGER VALUE FOR PERMISSION_CODE_WRITE LIKE 1002
+            } else {
+
+            }
+        }
+    }
+
+    private fun pickImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        register.launch(intent)
     }
 }
