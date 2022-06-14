7478a5b9b8d0634a917e8d158d3b97554903dca2
Cristian Arcadi <arduoscri@gmail.com>
2022-06-14 14:53:50 +0200

    Funzione per resize immagine profilo

--- app/src/main/java/com/eco/app/ProfileFragment.kt
+++ app/src/main/java/com/eco/app/ProfileFragment.kt
@@ -2,13 +2,14 @@ package com.eco.app
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,15 +17,15 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.PermissionChecker
 import androidx.core.content.PermissionChecker.checkSelfPermission
-import com.eco.app.databinding.ActivityProfileBinding
+import androidx.fragment.app.Fragment
 import com.eco.app.databinding.FragmentProfileBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
@@ -35,7 +36,8 @@ class ProfileFragment : Fragment() {
         if (it.resultCode == Activity.RESULT_OK) {
             Toast.makeText(requireContext(), "APPOSTO REGISTER LAUNCHER", Toast.LENGTH_SHORT).show()
             val imguri: Uri? = it.data?.data
-            binding.imgProfile.setImageURI(imguri)
+            val imgBitmap : Bitmap? = context?.let { it1 -> decodeUri(it1,imguri,230) }
+            binding.imgProfile.setImageBitmap(imgBitmap)
         }
     }
 
@@ -63,7 +65,25 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
-    private fun getInfos(usersReference : DatabaseReference, UID : String) {
+    fun decodeUri(c: Context, uri: Uri?, requiredSize: Int): Bitmap? {
+        val o = BitmapFactory.Options()
+        o.inJustDecodeBounds = true
+        BitmapFactory.decodeStream(uri?.let { c.getContentResolver().openInputStream(it) }, null, o)
+        var width_tmp = o.outWidth
+        var height_tmp = o.outHeight
+        var scale = 1
+        while (true) {
+            if (width_tmp / 2 < requiredSize || height_tmp / 2 < requiredSize) break
+            width_tmp /= 2
+            height_tmp /= 2
+            scale *= 2
+        }
+        val o2 = BitmapFactory.Options()
+        o2.inSampleSize = scale
+        return BitmapFactory.decodeStream(uri?.let { c.getContentResolver().openInputStream(it) }, null, o2)
+    }
+
+    private fun getInfos(usersReference : DatabaseReference, UID : String) { //todo finire con regole aperte
         usersReference.child(UID).get().addOnSuccessListener {
             val username : CharSequence = it.child("username").value as CharSequence
             val binScore : Long= it.child("bin_score").value as Long
