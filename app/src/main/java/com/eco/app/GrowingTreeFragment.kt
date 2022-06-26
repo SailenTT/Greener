a3bbc00afccb2b7ea5fe96bbe9f5538404f52b2c
David Poletti <david.poletti3@gmail.com>
2022-06-26 16:42:13 +0200

    Aggiunto innaffiatoio in GrowingTreeGame e sistemato bug collisioni in TrashBinGame

--- app/src/main/java/com/eco/app/GrowingTreeFragment.kt
+++ app/src/main/java/com/eco/app/GrowingTreeFragment.kt
@@ -1,5 +1,7 @@
 package com.eco.app
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -7,10 +9,12 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.airbnb.lottie.LottieAnimationView
 import com.eco.app.databinding.FragmentGrowingTreeBinding
 
 class GrowingTreeFragment : Fragment(),SensorEventListener {
@@ -19,16 +23,76 @@ class GrowingTreeFragment : Fragment(),SensorEventListener {
     private var running : Boolean = false
     private var totalSteps : Float= 0f
     private var previousTotalSteps : Float = 0f
+    private lateinit var wateringCan: LottieAnimationView
+    private var startX: Float = 0F
+    private var startY: Float = 0F
+    private val minFrame=23
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding=FragmentGrowingTreeBinding.inflate(inflater,container,false)
         sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager //getto il sensore
+
+        wateringCan=binding.wateringCan
+
+        wateringCan.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {}
+
+            override fun onAnimationEnd(animation: Animator?) {
+                wateringCan.setMinFrame(minFrame)
+                wateringCan.progress = 0F
+                wateringCan.playAnimation()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {}
+
+            override fun onAnimationStart(animation: Animator?) {}
+        })
+
+        wateringCan.setOnTouchListener { v, event ->touchListener(v,event)  }
+
         return binding.root
     }
+
+    fun touchListener(v: View, event: MotionEvent): Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN->{
+                startX = event.x
+                startY = event.y
+                return true
+            }
+            MotionEvent.ACTION_MOVE->{
+                val deltaX = event.x - startX
+                val deltaY = event.y - startY
+                wateringCan.x += deltaX
+                wateringCan.y += deltaY
+                if (wateringCan.x >= (binding.wateringCanContainer.x + binding.wateringCanContainer.width)
+                    ||wateringCan.y>= (binding.wateringCanContainer.y + binding.wateringCanContainer.height)
+                    ||wateringCan.y+wateringCan.height<=binding.wateringCanContainer.y){
+
+                    if (!wateringCan.isAnimating) {
+                        wateringCan.playAnimation()
+                    }
+                }
+                return true
+            }
+            MotionEvent.ACTION_UP-> {
+                //resetto la posizione dell'oggetto
+                wateringCan.cancelAnimation()
+                wateringCan.setMinFrame(0)
+                wateringCan.progress=0F
+                wateringCan.animate()
+                    .translationY(0F)
+                    .translationX(0F)
+                return true
+            }
+        }
+        return true
+    }
+
 
     override fun onResume() {
         super.onResume()
@@ -56,7 +120,5 @@ class GrowingTreeFragment : Fragment(),SensorEventListener {
        }
     }
 
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
 }
