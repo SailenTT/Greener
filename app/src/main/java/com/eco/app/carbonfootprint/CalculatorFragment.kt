5c945c69a4b3a33d158c2b260d84b94756034ba8
cristiandenicola <cristiandenicola009@gmail.com>
2022-07-08 18:44:05 +0200

    MODIFICA: miglioramenti vari e devo finire cfp

--- app/src/main/java/com/eco/app/carbonfootprint/CalculatorFragment.kt
+++ app/src/main/java/com/eco/app/carbonfootprint/CalculatorFragment.kt
@@ -27,31 +27,6 @@ class CalculatorFragment : Fragment() {
     //8 = bus (vale lo stesso concetto, più passeggeri si porta più si inquina)
     //10 = treno (valore massimo al mezzo che trasporta più persone (escluso aereo perchè ha una sezione propria))
 
-    // JourneyTime = 0 //variabile che conterrà quanto tempo l'utente ha passato in viaggio su uno dei mezzi scelti
-    //verrà usato come moltiplicatore in scaglioni
-
-    //var ticketShort = 0 //contatore ticket S
-    //tenendo conto che il treno prima ha preso il punteggio massimo a 10
-    //per ogni biglietto corto vengono aggiunti 15 punti (30 per andata e ritorno)
-
-    //var ticketMedium = 0 //contatore ticket M
-    //per ogni biglietto medio vengono aggiunti 20 punti (40 per andata e ritorno)
-
-    //var ticketLong = 0 //contatore ticket L
-    //per ogni biglietto lungo vengono aggiunti 25 punti (50 per andata e ritorno)
-
-    //var whoIsChecked = 0 //var che tiene conto di quale checkbox è true
-
-    //var FoodTrackingScore = 0 //variabile che tiene conto del punteggio dell'utente nella pagina "Food Tracking";
-    //anche per questo, il punteggio va in base a che dieta (mediamente) segue l'utente. anche qui abbiamo una divisione in scala decrescente:
-    //10 punti a carnePlus
-    //8 punti a carneMedia
-    //6 punti a carneLow
-    //4 punti a pescetarian
-    //3 punti a vetetarian
-    //2 punti a veggy
-    //il punteggio è basato su quanto la tua dieta possa produrre CO2 (con allevamenti, produzione etc)
-
     private lateinit var binding: FragmentCalculatorBinding
 
     //VAR "Journey Mode"
@@ -62,36 +37,6 @@ class CalculatorFragment : Fragment() {
     private lateinit var cardMoto : MaterialCardView
     private lateinit var cardWalk : MaterialCardView
     private lateinit var btnNextJM : Button
-
-    //VAR "Journey Time"
-    //private lateinit var timePick : TimePicker
-    //private lateinit var btnPrevJT : Button
-    //private lateinit var btnNextJT : Button
-
-    /*
-    //VAR "Your Flights"
-    private  lateinit var btnminusShort : Button
-    private  lateinit var txtvalShort : TextView
-    private  lateinit var btnplusShort : Button
-    private  lateinit var btnminusMedium : Button
-    private  lateinit var txtvalMedium : TextView
-    private  lateinit var btnplusMedium : Button
-    private  lateinit var btnminusLong : Button
-    private  lateinit var txtvalLong : TextView
-    private  lateinit var btnplusLong : Button
-    private lateinit var btnPrevYF : Button
-    private lateinit var btnNextYF : Button*/
-
-    /*
-    //VAR "Food Tracking"
-    private lateinit var checkMeatPlus : CheckBox
-    private lateinit var checkMeat : CheckBox
-    private lateinit var checkMeatLow : CheckBox
-    private lateinit var checkFish : CheckBox
-    private lateinit var checkVeget : CheckBox
-    private lateinit var checkVeggy : CheckBox
-    private lateinit var btnPrevFT : Button
-    private lateinit var btnNextFT : Button*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -106,40 +51,6 @@ class CalculatorFragment : Fragment() {
         cardMoto = binding.cvMoto
         cardWalk = binding.cvWalk
         btnNextJM = binding.btnNextJM
-
-        //BINDING "Journey Time"
-        //timePick = binding.timePicker
-        //btnPrevJT = binding.btnPrevJT
-        //btnNextJT = binding.btnNextJT
-
-        /*
-        //BINDING "Your Flights"
-        btnminusShort = binding.btnMinusCorto
-        txtvalShort = binding.txtNticketCorto
-        btnplusShort = binding.btnPlusCorto
-        btnminusMedium = binding.btnMinusMedio
-        txtvalMedium = binding.txtNticketMedio
-        btnplusMedium = binding.btnPlusMedio
-        btnminusLong = binding.btnMinusLungo
-        txtvalLong = binding.txtNticketLungo
-        btnplusLong = binding.btnPlusLungo
-        btnPrevYF = binding.btnPrevYF
-        btnNextYF = binding.btnNextYF*/
-
-        /*
-        //BINDING "Food Tracking"
-        checkMeatPlus = binding.checkMeatplus
-        checkMeat = binding.checkMeat
-        checkMeatLow = binding.checkMeatlow
-        checkFish = binding.checkFish
-        checkVeget = binding.checkVeget
-        checkVeggy = binding.checkVeggy
-        btnPrevFT = binding.btnPrevFT
-        btnNextFT = binding.btnNextFT*/
-
-        //btnNextFT.setOnClickListener {
-        //findNavController().navigate(CalculatorFragmentDirections.fromPage0ToPage1())
-        //}
 
         //Listener per settare checked le varie card presenti nella GridView
         cardAuto.setOnClickListener {
@@ -202,197 +113,19 @@ class CalculatorFragment : Fragment() {
             }
         }
 
-        //setto property per il timePicker della sezione "Journey Mode"
-        //timePick.setIs24HourView(true)
-        //timePick.hour = 0
-        //timePick.minute = 0
-
-        /*
-        //Listener dei btn per la sezione "VOLI"
-        btnminusShort.setOnClickListener {
-            ticketShort--
-
-            //controllo del valore:
-            //se va negativo faccio tornare a 0 cosi da avere sempre un valore positivo.
-            if (ticketShort<0)
-                ticketShort = 0
-
-            txtvalShort.text = ticketShort.toString()
-        }
-
-        btnplusShort.setOnClickListener {
-            ticketShort++
-            txtvalShort.text = ticketShort.toString()
-        }
-
-        btnminusMedium.setOnClickListener {
-            ticketMedium--
-
-            if (ticketMedium<0)
-                ticketMedium = 0
-
-            txtvalMedium.text = ticketMedium.toString()
-        }
-
-        btnplusMedium.setOnClickListener {
-            ticketMedium++
-            txtvalMedium.text = ticketMedium.toString()
-        }
-
-        btnminusLong.setOnClickListener {
-            ticketLong--
-
-            if (ticketLong<0)
-                ticketLong = 0
-
-            txtvalLong.text = ticketLong.toString()
-        }
-
-        btnplusLong.setOnClickListener {
-            ticketLong++
-            txtvalLong.text = ticketLong.toString()
-        }*/
-
-        /*
-        //Listener per le checkbox del "food tracker"
-        checkMeatPlus.setOnClickListener {
-            if (checkMeatPlus.isChecked) {
-                //se la checkbox è selezionata salvo in whoIsChecked (0 la prima, ..., 5 l'ultima)
-                //dopo setto tutte le altre false in modo tale che solo una possa essere selezionabile
-                whoIsChecked = 0
-                FoodTrackingScore += 10
-                checkMeat.isChecked = false
-                checkMeatLow.isChecked = false
-                checkFish.isChecked = false
-                checkVeget.isChecked = false
-                checkVeggy.isChecked = false
-            }
-        }
-
-        checkMeat.setOnClickListener {
-            if (checkMeat.isChecked) {
-                whoIsChecked = 1
-                FoodTrackingScore += 8
-                checkMeatPlus.isChecked = false
-                checkMeatLow.isChecked = false
-                checkFish.isChecked = false
-                checkVeget.isChecked = false
-                checkVeggy.isChecked = false
-            }
-        }
-
-        checkMeatLow.setOnClickListener {
-            if (checkMeatLow.isChecked) {
-                whoIsChecked = 2
-                FoodTrackingScore += 6
-                checkMeatPlus.isChecked = false
-                checkMeat.isChecked = false
-                checkFish.isChecked = false
-                checkVeget.isChecked = false
-                checkVeggy.isChecked = false
-            }
-        }
-
-        checkFish.setOnClickListener {
-            if (checkFish.isChecked) {
-                whoIsChecked = 3
-                FoodTrackingScore += 4
-                checkMeatPlus.isChecked = false
-                checkMeat.isChecked = false
-                checkMeatLow.isChecked = false
-                checkVeget.isChecked = false
-                checkVeggy.isChecked = false
-            }
-        }
-
-        checkVeget.setOnClickListener {
-            if (checkVeget.isChecked) {
-                whoIsChecked = 4
-                FoodTrackingScore += 3
-                checkMeatPlus.isChecked = false
-                checkMeat.isChecked = false
-                checkMeatLow.isChecked = false
-                checkFish.isChecked = false
-                checkVeggy.isChecked = false
-            }
-        }
-
-        checkVeggy.setOnClickListener {
-            if (checkVeggy.isChecked) {
-                whoIsChecked = 5
-                FoodTrackingScore += 2
-                checkMeatPlus.isChecked = false
-                checkMeat.isChecked = false
-                checkMeatLow.isChecked = false
-                checkFish.isChecked = false
-                checkVeget.isChecked = false
-            }
-        }*/
-
-        //funzione di prova per vedere se prende i valori correttamente
-        //TODO poi toglierla e inserire i dati nel calcolo finale
         btnNextJM.setOnClickListener{
-
             var sum = calcolaCO2()
-            findNavController().navigate(CalculatorFragmentDirections.fromPage0ToPage1())
-            //Bundle().putDouble("risultato", sum)
-            //Snackbar.make(it, "$sum totale", Snackbar.LENGTH_LONG).show()
+            findNavController().navigate(CalculatorFragmentDirections.fromPage0ToPage1(sum))
         }
 
         return binding.root
     }
 
-    fun  calcolaCO2(): Double {
-        var sumTotale : Double = 0.0
+    fun  calcolaCO2(): Float {
+        var sumTotale : Float = 0.0F
 
-        //per prima cosa aggiungo il punteggio del "Journey Mode"
         sumTotale += JourneyModeScore
 
-        /*
-        var ore = timePick.hour
-        var minuti = timePick.minute
-
-        if(minuti>=30) {
-            ore += 1 //approssimo
-        }
-
-        if(ore <= 2) { // scaglione basso
-            sumTotale *= 1.5
-
-        }
-        else if (ore in 3..5) { // scaglione intermedio
-            sumTotale *= 2.5
-        }
-        else { //scaglione massimo
-            sumTotale *= 3.5
-        }*/
-
-        //ora aggiungo la parte di your flight
-        //sumTotale += (ticketShort * 15.0) + (ticketMedium * 20.0) + (ticketLong * 25.0)
-
-        /*
-        //infine aggiungo la parte del food tracking
-        when (whoIsChecked) {
-            0 -> {
-                sumTotale += 10.0
-            }
-            1 -> {
-                sumTotale += 8.0
-            }
-            2 -> {
-                sumTotale += 6.0
-            }
-            3 -> {
-                sumTotale += 4.0
-            }
-            4 -> {
-                sumTotale += 3.0
-            }
-            5 -> {
-                sumTotale += 2.0
-            }
-        }*/
         return sumTotale
     }
-
 }
--- app/src/main/java/com/eco/app/carbonfootprint/ResultCalculatorFragment.kt
+++ app/src/main/java/com/eco/app/carbonfootprint/ResultCalculatorFragment.kt
@@ -1,60 +1,141 @@
 package com.eco.app.carbonfootprint
 
+import android.R
+import android.app.ActionBar
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.eco.app.R
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.eco.app.databinding.FragmentResultCalculatorBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ResultCalculator.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ResultCalculator : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val args : ResultCalculatorArgs by navArgs()
+
+    val Tips = Array<String>(10) { "Spegni il motore al semaforo se è rosso"; "Diminuisci l'uso di plastica in casa"; "Cambia stile di alimentazione: prova con meno carne e più pesce"; "Prendi l'aereo solo se è strettamente necessario, bisogna preferire le altre alternative";"Riduci l'uso di imballaggi e gli oggetti monouso";"Anche se costano qualcosina in più, preferisci sempre gli elettrodomestici di classe A o A+";"Limita al prettamente necessario l'uso dei riscaldamenti";"Fai la raccolta differenziata (il nostro calendario può aiutarti!)";"Scegli prodotti di bellezza ecosostenibili!";"Lo sapevi che: Specialmente alcune tipologie di carne, come quelle di manzo e di agnello, emettono quantità ingenti di gas metano?";"scegli sempre (se possibile) un mezzo di trasporto a impatto ridotto: il treno sarebbe sempre da preferire all’aereo!"}
+    val rnds = (0..10).random()
+
+    private lateinit var binding: FragmentResultCalculatorBinding
+
+    private lateinit var txt_showResult : TextView
+    private lateinit var circleUtente : RelativeLayout
+    private lateinit var txt_tipsUtente : TextView
+
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result_calculator, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        binding = FragmentResultCalculatorBinding.inflate(inflater, container, false)
+
+        txt_showResult = binding.txtResShow
+        circleUtente = binding.cirlceUtente
+        txt_tipsUtente = binding.txtTipsUtente
+
+        //calcola()
+        //txt_tipsUtente.text = Tips[rnds]
+
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ResultCalculator.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ResultCalculator().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    fun calcola() {
+        val AVGmondiale = 432 //110
+        val punteggio_utente = args.sumFoodTracking //160
+
+        val view: View = binding.cirlceUtente
+        val layoutParams: RelativeLayout.LayoutParams = view.layoutParams as RelativeLayout.LayoutParams
+
+        //AVGmondiale (dato preso da internet) è 432 con punteggio di 60
+        //il calcolo funzionerà quindi con un compare del punteggio utente su questo valore;
+
+        /* scala valore
+           0 -> 0 kg 20
+           1-10 -> 8 kg 40
+           11 - 20 -> 73 kg 50
+           21 - 30 -> 176 kg 60
+           31 - 40 -> 221 kg 70
+           41 - 50 -> 352 kg 100
+           51 - 60 -> 432 kg 110
+           61 - 70 -> 584 kg 120
+           71 - 80 -> 657 kg 140
+           81 - 90 -> 749 kg 150
+           91 + -> 859 kg 160
+        */
+
+        when {
+            punteggio_utente == 0.0F -> {
+                txt_showResult.text = "0"
+                layoutParams.width = 20
+                layoutParams.height = 20
+                view.layoutParams = layoutParams
             }
+            punteggio_utente in 1.0F..10.0F -> {
+                txt_showResult.text = "8"
+                layoutParams.width = 40
+                layoutParams.height = 40
+                view.layoutParams = layoutParams
+            }
+            punteggio_utente in 11.0F..20.0F -> {
+                txt_showResult.text = "73"
+                layoutParams.width = 50
+                layoutParams.height = 50
+                view.layoutParams = layoutParams
+            }
+            punteggio_utente in 21.0F..30.0F -> {
+                txt_showResult.text = "176"
+                layoutParams.width = 60
+                layoutParams.height = 60
+                view.layoutParams = layoutParams
+            }
+            punteggio_utente in 31.0F..40.0F -> {
+                txt_showResult.text = "221"
+                layoutParams.width = 70
+                layoutParams.height = 70
+                view.layoutParams = layoutParams
+            }
+            punteggio_utente in 41.0F..50.0F -> {
+                txt_showResult.text = "352"
+                layoutParams.width = 100
+                layoutParams.height = 100
+                view.layoutParams = layoutParams
+            }
+            punteggio_utente in 51.0F..60.0F -> {
+                txt_showResult.text = "432"
+                layoutParams.width = 110
+                layoutParams.height = 110
+                view.layoutParams = layoutParams
+            }
+            punteggio_utente in 61.0F..70.0F -> {
+                txt_showResult.text = "584"
+                layoutParams.width = 120
+                layoutParams.height = 120
+                view.layoutParams = layoutParams
+            }
+            punteggio_utente in 71.0F..80.0F -> {
+                txt_showResult.text = "584"
+                layoutParams.width = 140
+                layoutParams.height = 140
+                view.layoutParams = layoutParams
+            }
+            punteggio_utente in 81.0F..90.0F -> {
+                txt_showResult.text = "584"
+                layoutParams.width = 150
+                layoutParams.height = 150
+                view.layoutParams = layoutParams
+            }
+            punteggio_utente >= 91.0F -> {
+                txt_showResult.text = "859"
+                layoutParams.width = 160
+                layoutParams.height = 160
+                view.layoutParams = layoutParams
+            }
+        }
+
     }
 }
